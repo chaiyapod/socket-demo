97cf6611097ec7910e47fbdf835afcd090297075
chaiyapod tuntimanigun <earth.chaiyapod@gmail.com>
2023-07-01 15:18:55 +0700

    add log

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  console.log(
+    `application running on port : ${3000} and url is ${await app.getUrl()}`,
+  );
 }
 bootstrap();
